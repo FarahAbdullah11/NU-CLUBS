35f19222db58da35c8e5ab45a1f6df381f902ff0
habibakhaled-20 <h.khaled2340@nu.edu.eg>
2025-11-27 17:55:05 +0200

    add the navbar to the home page

---
+++ src/components/navbar.tsx
@@ -0,0 +1,53 @@
+import { Link, useLocation } from 'react-router-dom'
+import './navbar.css'
+
+const NavigationBar = () => {
+  const location = useLocation()
+
+  return (
+    <nav className= "navbar">
+      <div className="navbar-contet">
+        {/*Logo section*/}
+              <div className="navbar-logo">
+                <div className="logo-container">
+                    <div className="logo-monogram">Nu</div>
+                    <div className="logo-text">
+                        <div className="logo-english">Nile University</div>
+                        <div className="logo-arabic">جامعة النيل </div>
+                    </div>
+                </div>
+              </div>
+
+              {/*Clubs heading */}
+              <div className="navbar-heading">Clubs</div>
+              
+              {/* Navigation Links */}
+        <div className="navbar-links">
+          <Link 
+            to="/home"
+            className={`nav-link ${location.pathname === '/home' ? 'active' : ''}`}
+          >
+            Home
+          </Link>
+          <Link 
+            to="/request" 
+            className={`nav-link ${location.pathname === '/request' ? 'active' : ''}`}
+          >
+            Request
+          </Link>
+          <Link 
+            to="/profile" 
+            className={`nav-link ${location.pathname === '/profile' ? 'active' : ''}`}
+          >
+            Profile
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavigationBar
+                          
+        
+
